6e3778ba374a0305a808c7b64339d650690cdf61
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2026-01-14 09:02:07 +0100

    wip
    regs
    
    reorder BB
    
    rax
    
    rax
    
    rbx
    
    rdx -> r11
    
    no rdx
    
    f
    
    Hacks

--- src/coreclr/jit/lower.cpp
+++ src/coreclr/jit/lower.cpp
@@ -7505,6 +7505,18 @@ GenTree* Lowering::LowerVirtualVtableCall(GenTreeCall* call)
 // Lower stub dispatched virtual calls.
 GenTree* Lowering::LowerVirtualStubCall(GenTreeCall* call)
 {
+    GenTreeCall * temp = m_compiler->gtNewHelperCallNode(CORINFO_HELP_INTERFACELOOKUP_FOR_SLOT, TYP_I_IMPL);
+
+    call->gtCallAddr = temp->gtCallAddr;
+    call->gtCallType          = CT_USER_FUNC;
+    call->gtFlags &= ~GTF_CALL_VIRT_STUB;
+
+    CORINFO_CONST_LOOKUP helperLookup = m_compiler->compGetHelperFtn(CORINFO_HELP_INTERFACELOOKUP_FOR_SLOT);
+    call->gtDirectCallAddress         = helperLookup.addr;
+
+    return nullptr;
+
+#if 0
     assert(call->IsVirtualStub());
 
     // An x86 JIT which uses full stub dispatch must generate only
@@ -7587,6 +7599,7 @@ GenTree* Lowering::LowerVirtualStubCall(GenTreeCall* call)
 
     // TODO-Cleanup: start emitting random NOPS
     return result;
+#endif
 }
 
 //------------------------------------------------------------------------
